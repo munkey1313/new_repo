25ea8cc1d3c798a5632b2ce70e47e7a0d9c32056
Aaron Venezia <aaronvenezia@weber.edu>
2014-01-13 18:14:11 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FantasyImmortals::Application.config.secret_key_base = '77dcd6cccd20f1e0a447459a3356a6c79648997ff928401c5f784544c2ae7030baddf657626ccae083bfbac25d0c8b3fb1a9cfe0d5cbdbf9140c514b94c500c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FantasyImmortals::Application.config.session_store :cookie_store, key: '_fantasyImmortals_session'
