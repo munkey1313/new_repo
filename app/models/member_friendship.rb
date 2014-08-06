d2e65622e3e894486a40948d89a683e0c1db28fd
Randie Ellis <ellisfam1313@gmail.com>
2014-08-05 23:42:54 -0600

    removed attr_accessible from models/member_friendship.rb

--- app/models/member_friendship.rb
+++ app/models/member_friendship.rb
@@ -2,5 +2,5 @@ class MemberFriendship < ActiveRecord::Base
 	belongs_to :member 
 	belongs_to :friend, class_name: 'Member', foreign_key: 'friend_id'
 
-	attr_accessible :member, :friend, :member_id, :friend_id
+	#attr_accessible :member, :friend, :member_id, :friend_id
 end
